9ddff2977b48c96f5a244a6097072c511acd5f3f
Micah Lynn <micah@sabado.com>
2020-01-24 02:09:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,5 +20,7 @@ function sayHiToGrandma(string) {
   if (string === 'hello')
     return "I can't hear you!"
   else if (string === 'HELLO')
-    return 
+    return "YES INDEED!"
+  else if (string === 'I love you, Grandma.')
+    return "I love you, too."
 }
